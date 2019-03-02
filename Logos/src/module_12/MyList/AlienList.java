5d21785b2198d995e1e4ca5adf90a4782df7ace9
Valera3107 <valerapetruna3107@gmail.com>
2019-03-02 18:02:46 +0200

    some fixing

--- Logos/src/module_12/MyList/AlienList.java
+++ Logos/src/module_12/MyList/AlienList.java
@@ -2,67 +2,40 @@ package module_12.MyList;
 
 import java.util.Arrays;
 
-public class AlienList {
-  private static final Object[] DEFAULT_CAPACITY = new Object[10];
-  private static final Object[] EMPTY_ELEMENT_DATA = {};
-  private Object[] array;
+public class AlienList<T> {
+  private int arrSize = 8;
+  private int counter = 0;
+  private T[] arr = (T[]) new Object[arrSize];
 
-  public AlienList(int initialCapacity) {
-    if(initialCapacity > 0){
-      array = new Object[initialCapacity];
-    } else if(initialCapacity == 0){
-      array = EMPTY_ELEMENT_DATA;
+
+  public void add(T item) {
+    if (counter < arr.length) {
+      arr[counter] = item;
+      counter++;
     } else {
-      throw new IllegalArgumentException("Illegal Capacity: "+
-        initialCapacity);
+      arrSize += 8;
+      T[] extendedArr = (T[]) new Object[arrSize];
+      System.arraycopy(arr, 0, extendedArr, 0, arr.length);
+      arr = extendedArr;
+      arr[counter] = item;
+      counter++;
     }
   }
 
-  public AlienList(){
-    array = DEFAULT_CAPACITY;
-  }
-
-  public boolean add(Object item){
-    boolean isAdded = false;
-    Object[] additionalArray = new Object[array.length*2];
-    int addIndex = 0;
-    for (int i = 0; i < array.length; i++) {
-      if(array[i] == null){
-        addIndex = i;
-        isAdded = true;
-      }
-    }
-    array[addIndex] = item;
-    if (isAdded){
-      return true;
-    } else {
-      System.arraycopy(array, 0, additionalArray, 0, array.length);
-      array = additionalArray;
-    }
-    return false;
-  }
-
-  public  boolean delete(int index) {
-    if(array[index] != null) {
-      array[index] = null;
-      array = removeElement(array, index);
-      return true;
-    }
-    return false;
-  }
-
-  public static Object[] removeElement( Object[] arr, int index ){
-    Object[] arrOut = new Object[arr.length - 1];
-    int remainingElements = arr.length - ( index + 1 );
-    System.arraycopy(arr, 0, arrOut, 0, index);
-    System.arraycopy(arr, index + 1, arrOut, index, remainingElements);
-    return arrOut;
+  public void remove(int index) {
+    arr[index] = null;
   }
 
   @Override
   public String toString() {
-    return "AlienList{" +
-      "array=" + Arrays.toString(array) +
-      '}';
+    StringBuilder sb = new StringBuilder("");
+    for (int i = 0; i < counter; i++) {
+      if (arr[i] != null)
+        sb.append(arr[i]).append(" ");
+    }
+    return sb.toString();
   }
+
+
+
 }
