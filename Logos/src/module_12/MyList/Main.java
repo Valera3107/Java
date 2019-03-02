530da431c55c6ef4ccd0c463e51f4d9294290ac0
Valera3107 <valerapetruna3107@gmail.com>
2019-03-02 12:30:57 +0200

    some fixing

--- Logos/src/module_12/MyList/Main.java
+++ Logos/src/module_12/MyList/Main.java
@@ -1,28 +1,41 @@
 package module_12.MyList;
 
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    AlienList alienList = new AlienList();
-    alienList.add("rrr");
-    alienList.add("ntdi");
-    alienList.add("xnrf");
-    alienList.add("jnrtg");
-    alienList.add("crg");
-    alienList.add("crt");
-    alienList.add("xtxt");
-    alienList.add("rtvh");
-    alienList.add("cntuc");
-    alienList.add("ccc");
-    alienList.add("cun4");
-    alienList.add("wcgtgc");
+//    AlienList alienList = new AlienList();
+//    alienList.add("rrr");
+//    alienList.add("ntdi");
+//    alienList.add("xnrf");
+//    alienList.add("jnrtg");
+//    alienList.add("crg");
+//    alienList.add("crt");
+//    alienList.add("xtxt");
+//    alienList.add("rtvh");
+//    alienList.add("cntuc");
+//    alienList.add("ccc");
+//    alienList.add("cun4");
+//    alienList.add("wcgtgc");
+//
+//    System.out.println(alienList.toString());
+//
+//    alienList.delete(4);
+//    alienList.delete(10);
+//
+//    System.out.println(alienList.toString());
 
-    System.out.println(alienList.toString());
+    ToDoList<String> list = new ToDoList<>();
+    list.add("Gym");
+    list.add("Eating");
+    list.add("Studying");
+    list.add("Walking");
 
-    alienList.delete(4);
-    alienList.delete(10);
+    System.out.println(list);
 
-    System.out.println(alienList.toString());
 
+    list.remove(1);
+
+    System.out.println(list);
   }
 }
